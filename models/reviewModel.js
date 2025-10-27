74b763fd367b69fc06f851f2ac5f51042a836edb
Rishikesh Negi <rishinegi97@gmail.com>
2025-10-27 19:10:37 +0530

    Add aggregated data of related data set to main data set

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const Tour = require("./tourModel");
 
 const reviewSchema = new mongoose.Schema(
   {
@@ -55,6 +56,47 @@ reviewSchema.pre(/^find/, function (next) {
   });
 
   next();
+});
+
+// Creating a static method on the Review model using the "statics" property of the Schema:
+reviewSchema.statics.calcAverageRating = async function (tourId) {
+  const stats = await this.aggregate([
+    {
+      $match: { tour: tourId },
+      $group: {
+        _id: "$tour",
+        nRating: { $sum: 1 },
+        avgRating: { $avg: "$rating" },
+      },
+    },
+  ]);
+
+  await Tour.findByIdAndUpdate(tourId, {
+    ratingsQuantity: stats[0]?.nRating || 0,
+    ratingsAverage: stats[0]?.avgRating || 4.5,
+  });
+};
+
+reviewSchema.post("save", function () {
+  // "this" points to the review document being created:
+  Review.calcAverageRating(this.tour);
+
+  // At this point, the Review model has not been initialized, so we cannot directly reference it. We also cannot place this middleware after the declaration of the Review model because the Express code runs sequentially, and the review schema will not contain any middlewares that are defined after the review model is created. Instead, we can use this.constructor to reference it because the Review model is indeed the constructor of each review document:
+  this.constructor.calcAverageRating(this.tour);
+});
+
+// In query middlewares like findByIdAndUpdate and findByIdAndDelete, we don't get direct access to the current document. So, we can use a workaround to access the current document in order to update the stats of the main data set when the related data set is updated using the mentioned queries:
+reviewSchema.pre(/^findOneAnd/, async function (next) {
+  // In a "pre" middleware, "this" points to the query, not to the document. So, to get the current document, we can call the findOne() method on the query. Additionally, to pass the document into a "post" middleware for recalculation of the stats, after the document or its updates have been saved, we can attach it to the query by creating a property on the query:
+  this.reviewDoc = await this.findOne(); // The reviewDoc property can now be used in a "post" middleware to access the document and its data
+
+  next();
+});
+// In the above middleware, we could not have used "post" to calculate the aggregated ratings data because the "post" middlewares don't have access to the query (findOne, in this case) that we use to access the document. On the other hand, the "pre" middleware will not account for the new or updated document, because the document is yet to be saved/updated in the database. So, the solution is to create a "post" middleware that can access the document data that was attached to the query object by the "pre" middleware:
+
+reviewSchema.post(/^findOneAnd/, async function () {
+  // In a "post" middleware, "this" still points to the query. However, by the time the query reaches a "post" middleware, it is already executed. So, we could not use the .findOne() method on it to get the document, like we did in the above "pre" query middleware:
+  await this.reviewDoc.constructor.calcAverageRating(this.reviewDoc.tour);
 });
 
 const Review = mongoose.model("Review", reviewSchema);
