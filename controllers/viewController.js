f29b291ad417033c946199b14394a8a068ea50c0
Rishikesh Negi <rishinegi97@gmail.com>
2025-12-02 17:27:08 +0530

    Create handler for the tour details page view

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -6,8 +6,10 @@ exports.getOverview = catchAsync(async (req, res, next) => {
   const tours = await Tour.find();
 
   // 2) Build template
+  // Done in overview.pug
 
   // 3) Render the template using the fetched data:
+  // Done in overview.pug
 
   res.status(200).render("overview", {
     title: "All Tours",
@@ -15,8 +17,14 @@ exports.getOverview = catchAsync(async (req, res, next) => {
   });
 });
 
-exports.getTourDetails = (req, res) => {
-  res.status(200).render("tour", {
-    title: "The Forest Hiker Tour",
+exports.getTourDetails = catchAsync(async (req, res) => {
+  const tour = await Tour.findOne({ slug: req.params.slug }).populate({
+    path: "reviews",
+    fields: "review rating author",
   });
-};
+
+  res.status(200).render("tour", {
+    title: tour.name,
+    tour,
+  });
+});
