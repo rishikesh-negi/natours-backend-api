12738c46183aa4491d183f1db34b45bf2226db10
Rishikesh Negi <rishinegi97@gmail.com>
2025-12-05 17:48:39 +0530

    Add route for login page

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -3,12 +3,14 @@ const express = require("express");
 const {
   getOverview,
   getTourDetails,
+  getLoginPage,
 } = require("../controllers/viewController");
 
 const router = express.Router();
 
 // Views (pug template) routes for rendering views:
 router.get("/", getOverview);
+router.get("/login", getLoginPage);
 router.get("/tour/:slug", getTourDetails);
 
 module.exports = router;
