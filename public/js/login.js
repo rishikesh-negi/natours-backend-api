68a54a1566bd648ae99bb42ec797e581276c1fd7
Rishikesh Negi <rishinegi97@gmail.com>
2025-12-08 18:23:02 +0530

    Add logout function to client-side script

--- public/js/login.js
+++ public/js/login.js
@@ -1,7 +1,7 @@
 /* eslint-disable */
 import "@babel/polyfill";
 import axios from "axios";
-import { showAlert } from "./alerts";
+import { hideAlert, showAlert } from "./alerts";
 
 export const login = async function (email, password) {
   try {
@@ -20,5 +20,24 @@ export const login = async function (email, password) {
     }
   } catch (err) {
     showAlert("error", err.response.data.message);
+  }
+};
+
+export const logout = async function () {
+  try {
+    const res = await axios({
+      method: "GET",
+      url: "http://localhost:8000/api/v1/users/logout",
+    });
+
+    if (res.data.status === "success") {
+      showAlert("success", "Logged out successfully!");
+      setTimeout(() => {
+        hideAlert();
+        location.reload(true);
+      }, 1000);
+    }
+  } catch (err) {
+    showAlert("error", "Error logging out! Try again.");
   }
 };
