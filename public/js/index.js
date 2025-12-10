34c60c2270ec3e3fd756439936c85a3069e00418
Rishikesh Negi <rishinegi97@gmail.com>
2025-12-10 17:20:14 +0530

    Add client-side handler to update user data on form submit

--- public/js/index.js
+++ public/js/index.js
@@ -1,10 +1,12 @@
 /* eslint-disable */
 import { displayMap } from "./mapbox";
 import { login, logout } from "./login";
+import { updateUserData } from "./updateSettings";
 
 // DOM Elements:
 const mapBox = document.getElementById("map");
-const loginForm = document.querySelector(".form");
+const loginForm = document.querySelector(".form--login");
+const updateUserDataForm = document.querySelector(".form-user-data");
 const logoutBtn = document.querySelector(".nav__el--logout");
 
 // Form values:
@@ -23,6 +25,17 @@ if (loginForm) {
     email = document.getElementById("email").value;
     password = document.getElementById("password").value;
     login(email, password);
+  });
+}
+
+if (updateUserDataForm) {
+  updateUserDataForm.addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    const name = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+
+    updateUserData(name, email);
   });
 }
 
