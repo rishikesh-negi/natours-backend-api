62d44725322260d5c638aa3b1c2f4b7d0595301d
Rishikesh Negi <rishinegi97@gmail.com>
2025-12-20 18:28:03 +0530

    Create controller to resize user-uploaded avatar using the sharp library

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,5 @@
 const multer = require("multer");
+const sharp = require("sharp");
 
 const User = require("../models/userModel");
 const AppError = require("../utils/appError");
@@ -6,20 +7,33 @@ const { deleteOne, updateOne, getOne, getAll } = require("./handlerFactory");
 
 const catchAsync = require("../utils/catchAsync");
 
-// Configuring Multer storage and filter for file uploads. The "cb" parameter in the functions below is a callback function somewhat similar to Express's "next" function:
-const multerStorage = multer.diskStorage({
-  destination(req, file, cb) {
-    // To specify the destination and on-error operation, we use the "cb" callback. The first parameter represents any possible errors.:
-    cb(null, "public/img/users");
-  },
-  filename(req, file, cb) {
-    // Get image extension from mimetype header (Ex: image/jpeg):
-    const ext = file.mimetype.split("/").at(1);
+const filterObj = function (obj, ...allowedFields) {
+  const filteredObj = {};
 
-    // Store file to the file system after naming it:
-    cb(null, `user-${req.user.id}-${Date.now()}.${ext}`);
-  },
-});
+  Object.keys(obj).forEach((key) => {
+    if (allowedFields.includes(key)) filteredObj[key] = obj[key];
+  });
+
+  return filteredObj;
+};
+
+// Configuring Multer storage and filter for file uploads. The "cb" parameter in the functions below is a callback function somewhat similar to Express's "next" function:
+// const multerStorage = multer.diskStorage({
+//   destination(req, file, cb) {
+//     // To specify the destination and on-error operation, we use the "cb" callback. The first parameter represents any possible errors.:
+//     cb(null, "public/img/users");
+//   },
+//   filename(req, file, cb) {
+//     // Get image extension from mimetype header (Ex: image/jpeg):
+//     const ext = file.mimetype.split("/").at(1);
+
+//     // Store file to the file system after naming it:
+//     cb(null, `user-${req.user.id}-${Date.now()}.${ext}`);
+//   },
+// });
+
+// Temporarily save uploaded image to memory, instead of the disk, for image processing and manipulation:
+const multerStorage = multer.memoryStorage();
 
 const multerFilter = function (req, file, cb) {
   // Allow only images in the upload:
@@ -35,17 +49,21 @@ const upload = multer({
   fileFilter: multerFilter,
 });
 
-const filterObj = function (obj, ...allowedFields) {
-  const filteredObj = {};
-
-  Object.keys(obj).forEach((key) => {
-    if (allowedFields.includes(key)) filteredObj[key] = obj[key];
-  });
-
-  return filteredObj;
-};
-
 exports.uploadUserPhoto = upload.single("photo");
+
+exports.resizeUserPhoto = function (req, res, next) {
+  if (!req.file) return next();
+
+  req.file.filename = `user-${req.user.id}-${Date.now()}.jpeg`;
+
+  sharp(req.file.buffer)
+    .resize(500, 500)
+    .toFormat("jpeg")
+    .jpeg({ quality: 90 })
+    .toFile(`public/img/users/${req.file.filename}`);
+
+  next();
+};
 
 exports.getMe = catchAsync(async function (req, res, next) {
   req.params.id = req.user.id;
@@ -64,6 +82,8 @@ exports.updateMe = catchAsync(async function (req, res, next) {
 
   // 2) Filter the request body to keep only the fields that are allowed to change:
   const filteredBody = filterObj(req.body, "name", "email");
+  // If the user changed their avatar, add the new file name to the document:
+  if (req.file) filteredBody.photo = req.file.filename;
 
   if (!Object.keys(filteredBody).length)
     return next(new AppError("No valid data to update found!", 400));
