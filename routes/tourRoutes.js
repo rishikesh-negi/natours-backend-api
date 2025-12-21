0b622284afcce5515f3cdaa1558589d78956cc2e
Rishikesh Negi <rishinegi97@gmail.com>
2025-12-21 18:43:36 +0530

    Add image upload and processing middlewares to the update tour stack

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -17,6 +17,8 @@ const {
   getMonthlyPlan,
   getToursWithin,
   getDistances,
+  uploadTourImages,
+  resizeTourImages,
 } = tourController;
 
 // Nesting the Review Routes Into the Tour Route:
@@ -41,7 +43,13 @@ router
 router
   .route("/:id")
   .get(getTour)
-  .patch(protect, restrictTo("admin", "lead-guide"), updateTour)
+  .patch(
+    protect,
+    restrictTo("admin", "lead-guide"),
+    uploadTourImages,
+    resizeTourImages,
+    updateTour,
+  )
   .delete(protect, restrictTo("admin", "lead-guide"), deleteTour);
 
 module.exports = router;
