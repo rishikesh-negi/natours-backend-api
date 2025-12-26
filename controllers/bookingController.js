679e5ce34f6235967a1cd48804c78ef5be96ba69
Rishikesh Negi <rishinegi97@gmail.com>
2025-12-26 19:00:04 +0530

    Create bookings route handlers to handle stripe checkout sessions and payments

---
+++ controllers/bookingController.js
@@ -0,0 +1,45 @@
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+
+const catchAsync = require("../utils/catchAsync");
+const factory = require("./handlerFactory");
+const AppError = require("../utils/appError");
+
+const Tour = require("../models/tourModel");
+
+exports.getCheckoutSession = catchAsync(async function (req, res) {
+  // 1) Get the tour being booked:
+  const tour = await Tour.findById(req.params.tourId);
+
+  // 2) Create the checkout session:
+  const session = await stripe.checkout.sessions.create({
+    payment_method_types: ["card"],
+    success_url: `${req.protocol}://${req.get("host")}/`,
+    cancel_url: `${req.protocol}://${req.get("host")}/tour/${tour.slug}`,
+    customer_email: req.user.email,
+    mode: "payment",
+
+    // Passes some data about the session. Gives us access to the session object after a successful transaction, allowing us to create a booking in the DB:
+    client_reference_id: req.params.tourId,
+
+    line_items: [
+      {
+        price_data: {
+          currency: "usd",
+          product_data: {
+            name: `${tour.name} Tour`,
+            description: tour.summary,
+            images: [`https://natours.dev/img/tours/${tour.imageCover}`],
+          },
+          unit_amount_decimal: tour.price * 100,
+        },
+        quantity: 1,
+      },
+    ],
+  });
+
+  // 3) Send the checkout session as response to the client:
+  res.status(200).json({
+    status: "success",
+    session,
+  });
+});
