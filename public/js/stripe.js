33cc1f3e8aba7b05fef4501a6bec2bfbbdba0947
Rishikesh Negi <rishinegi97@gmail.com>
2025-12-27 18:43:00 +0530

    Create a script to handle Stripe trnsactions

---
+++ public/js/stripe.js
@@ -0,0 +1,26 @@
+import axios from "axios";
+import { showAlert } from "./alerts";
+
+const { loadStripe } = require("@stripe/stripe-js");
+
+const stripePromise = loadStripe(
+  "pk_test_51SiD2qPuVwn1MptjjGjuVSrEQeT5L7n84xOoIywg3YYmcsMfJwyJtwRzumSrFnl0I1o8IFHJvhJ8Wlvxx5KaoPir005bHYXYub",
+);
+
+export async function bookTour(tourId) {
+  try {
+    // 1) Get the checkout session from the API endpoint:
+    const {
+      data: { session },
+    } = await axios(
+      `http://localhost:8000/api/v1/bookings/checkout-session/${tourId}`,
+    );
+    console.log(session);
+
+    // 2) Create checkout form + charge credit card:
+    window.location.assign(session.url);
+  } catch (err) {
+    console.error(err);
+    showAlert("error", err.message);
+  }
+}
