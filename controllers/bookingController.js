a6e550172a41716753691f7214618e8cceb46208
Rishikesh Negi <rishinegi97@gmail.com>
2025-12-28 17:53:59 +0530

    Create a temporary handler to store bookings to the DB by accessing the booking data from the success URL of a Stripe checkout session

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -5,6 +5,7 @@ const factory = require("./handlerFactory");
 const AppError = require("../utils/appError");
 
 const Tour = require("../models/tourModel");
+const Booking = require("../models/bookingModel");
 
 exports.getCheckoutSession = catchAsync(async function (req, res) {
   // 1) Get the tour being booked:
@@ -13,7 +14,7 @@ exports.getCheckoutSession = catchAsync(async function (req, res) {
   // 2) Create the checkout session:
   const session = await stripe.checkout.sessions.create({
     payment_method_types: ["card"],
-    success_url: `${req.protocol}://${req.get("host")}/`,
+    success_url: `${req.protocol}://${req.get("host")}/?tour=${req.params.tourId}&user=${req.user.id}&price=${tour.price}`,
     cancel_url: `${req.protocol}://${req.get("host")}/tour/${tour.slug}`,
     customer_email: req.user.email,
     mode: "payment",
@@ -42,4 +43,19 @@ exports.getCheckoutSession = catchAsync(async function (req, res) {
     status: "success",
     session,
   });
+});
+
+// Temporary (and unsecure) solution for creating a booking in the DB after a successful checkout. Requires the storage of sensitive data in the URL, which is exposed to the client and can be used by any user to create a booking without making a payment. Used in development when the application is not yet deployed (no actual domain). Added to the "/" route in viewRoutes, because that is the checkout session's success URL. To be replaced by a real solution involving Stripe Webhooks after app deployment (when the real app domain is available):
+exports.createBookingCheckout = catchAsync(async (req, res, next) => {
+  const { tour, user, price } = req.query;
+
+  if (tour && user && price) {
+    await Booking.create({ tour, user, price });
+
+    // Redirect to the original URL but without the query data to render the home page normally:
+    res.redirect(req.originalUrl.split("?")[0]);
+  }
+
+  // If there is no tour, user, or price data in the URL query, simply call the next middleware in the "/" route's middleware stack in viewRoutes to render the home page in the usual way:
+  next();
 });
