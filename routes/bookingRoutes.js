3e8ead7b2708b3b841f9a707aaf3a86af0c7661e
Rishikesh Negi <rishinegi97@gmail.com>
2025-12-30 18:51:34 +0530

    Add routes and role-based authorization for CRUD operationsin the Bookings API

--- routes/bookingRoutes.js
+++ routes/bookingRoutes.js
@@ -1,12 +1,24 @@
 const express = require("express");
 
-const { getCheckoutSession } = require("../controllers/bookingController");
-const { protect } = require("../controllers/authController");
+const {
+  getCheckoutSession,
+  getAllBookings,
+  createBooking,
+  updateBooking,
+  deleteBooking,
+  getBooking,
+} = require("../controllers/bookingController");
+const { protect, restrictTo } = require("../controllers/authController");
 
 const router = express.Router();
 
 router.use(protect);
 
 router.get("/checkout-session/:tourId", getCheckoutSession);
+
+router.use(restrictTo("admin", "lead-guide"));
+
+router.route("/").get(getAllBookings).post(createBooking);
+router.route("/:id").get(getBooking).patch(updateBooking).delete(deleteBooking);
 
 module.exports = router;
