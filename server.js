ffdc7d2da3147c4b4da5de15f7372614d4fdeaf6
Rishikesh Negi <rishinegi97@gmail.com>
2026-01-02 19:05:08 +0530

    Move dotenv import to the top of the file

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+const mongoose = require("mongoose");
 
 // Handling Uncaught Exceptions Globally (Example: When a Non-existent Variable is Accessed):
 process.on("uncaughtException", function (err) {
