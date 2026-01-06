d17a20adaabd5e10597a56d3d3c35d1c0f45169d
Rishikesh Negi <rishinegi97@gmail.com>
2026-01-06 17:23:26 +0530

    Handle the SIGTERM signal for graceful shutdown and resource cleanup before process termination

--- server.js
+++ server.js
@@ -46,3 +46,38 @@ function gracefulShutdown(err, errorMessage) {
 process.on("unhandledRejection", function (err) {
   gracefulShutdown(err, "🔴 Unhandled rejection encountered! Shutting down...");
 });
+
+// Handle SIGTERM signals sent by hosting platforms to restart the service and keep it healthy:
+const shutdown = async (signal) => {
+  console.log(`\n🚦Received ${signal}. Shutting down gracefully...`);
+
+  // Stop accepting new connections:
+  server.close(async () => {
+    console.log("⛔HTTP server closed");
+
+    try {
+      // Close DB connection:
+      await mongoose.connection.close();
+      console.log("🧹Cleanup completed!💯");
+      process.exit(0);
+    } catch (err) {
+      console.error("Server shutdown", err);
+      process.exit(1);
+    }
+  });
+
+  // Safety fallback (Render allows ~30s):
+  setTimeout(() => {
+    console.error("Force shutdown");
+    process.exit(1);
+  }, 30000);
+};
+
+process.on("SIGTERM", shutdown);
+process.on("SIGINT", shutdown);
+// Why this is needed:
+// To restart the service and keep it healthy, Render sends a "SIGTERM" signal, waits ~30 seconds, sends a "SIGKILL" signal to kill the process if the app is still running. If the server shutdown is abrupt, any pending, unhandled requests will remain unhandled, and the data in the DB could corrupt.
+// So, we need to listen for "SIGTERM" and if the signal is detected:
+// 1. Stop accepting new traffic.
+// 2. Clean up resources.
+// 3. Exit cleanly before timeout.
